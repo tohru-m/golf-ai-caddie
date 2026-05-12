8b37a86ba70c14c6bd6c2d37dd8c72a9180c05ab
oruto <oruto203@gmail.com>
2026-05-12 19:23:58 +0900

    Expander見出しを30pxに拡大

--- app.py
+++ app.py
@@ -197,7 +197,7 @@ div[data-testid="stCheckbox"] input[type="checkbox"] {
 
 /* ===== Expander ===== */
 div[data-testid="stExpander"] summary {
-    font-size: 24px !important;
+    font-size: 30px !important;
     font-weight: 700;
 }
 
