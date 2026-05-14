9f202a1041a1c31e7f94d4545c5d7cc13a660bcf
oruto <oruto203@gmail.com>
2026-05-14 17:16:50 +0900

    ホールヘッダーのy／Par表示を32pxに変更

--- app_v2.py
+++ app_v2.py
@@ -249,7 +249,7 @@ div:has(#load-preset-anchor) + div[data-testid="stButton"] > button {
     white-space: nowrap;
 }
 .hole-header .sub {
-    font-size: 30px;
+    font-size: 32px;
     font-weight: 900;
     color: #93c5fd;
     white-space: nowrap;
